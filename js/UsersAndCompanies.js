998ace60e737271effd754da486eaca0e0571806
kpkmmk <mckuklina@gmail.com>
2019-07-01 11:57:38 +0200

    Commit

--- js/UsersAndCompanies.js
+++ js/UsersAndCompanies.js
@@ -5,18 +5,17 @@ let table = document.createElement('table');
 const tableDiv = document.querySelector('.tableBox');
 
 function getPromise(url) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', url);
-        xhr.onload = function() {
+        xhr.onload = function () {
             if (xhr.status == 200) {
                 resolve(JSON.parse(xhr.response));
-            }
-            else {
+            } else {
                 reject(Error(xhr.statusText));
             }
         };
-        xhr.onerror = function() {
+        xhr.onerror = function () {
             reject(Error("Network Error"));
         };
         xhr.send();
@@ -28,19 +27,18 @@ Promise.all([getPromise('http://localhost:3000/users'),
     users = values [0];
     comp = values[1];
 
-    for (let i = 0; i < comp.length; i++){
+    for (let i = 0; i < comp.length; i++) {
         let tr = document.createElement('tr');
         let td1 = document.createElement('td');
         let td2 = document.createElement('td');
         let text1 = document.createTextNode(String(comp[i].name));
-        let text3='';
+        let text3 = '';
         td1.appendChild(text1);
-        for(let j=0;j<users.length;j++){
-            if(users[j].uris.company===comp[i].uri){
-                if (text3===''){
-                    text3=text3 + String(users[j].name);
-                }
-                else {
+        for (let j = 0; j < users.length; j++) {
+            if (users[j].uris.company === comp[i].uri) {
+                if (text3 === '') {
+                    text3 = text3 + String(users[j].name);
+                } else {
                     text3 = text3 + (', ') + String(users[j].name);
                 }
             }
